b8535f36397fcc861a5c70abe50907ad504ff86e
EunBae Gong <geb9211@gmail.com>
2024-02-15 04:45:56 +0900

    chore: 빌드 환경설정

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -16,8 +16,8 @@ const config: Config = {
 
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
-	organizationName: "facebook", // Usually your GitHub org/user name.
-	projectName: "docusaurus", // Usually your repo name.
+	organizationName: "eunbae0", // Usually your GitHub org/user name.
+	projectName: "fe-store", // Usually your repo name.
 
 	onBrokenLinks: "throw",
 	onBrokenMarkdownLinks: "warn",
@@ -38,8 +38,7 @@ const config: Config = {
 					sidebarPath: "./sidebars.ts",
 					// Please change this to your repo.
 					// Remove this to remove the "edit this page" links.
-					editUrl:
-						"https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+					editUrl: "https://github.com/eunbae0/fe-store/tree/main/",
 				},
 				// blog: {
 				// 	showReadingTime: true,
@@ -87,7 +86,7 @@ const config: Config = {
 					items: [
 						{
 							label: "Basics",
-							to: "/docs/intro",
+							to: "/docs/basics/intro",
 						},
 					],
 				},
